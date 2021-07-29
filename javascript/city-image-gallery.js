ec647f516962cfcb7a81ed371c8f0484afb25745
Henry Cappel <hencappel1@gmail.com>
2021-07-29 17:20:30 -0500

    worked mostly on the galery page. Added the gallery carousel and info bar.

---
+++ javascript/city-image-gallery.js
@@ -0,0 +1,62 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const burger = document.querySelector('.burger');
+const slideInfo = document.querySelector('.content');
+
+
+const nextSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if(current.nextElementSibling) {
+        current.nextElementSibling.classList.add('current');
+    }
+    else {
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
+
+const prevSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if(current.previousElementSibling) {
+        current.previousElementSibling.classList.add('current');
+    }
+    else {
+        slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
+
+//Button events
+next.addEventListener('click', e => {
+    nextSlide();
+    if(auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+
+prev.addEventListener('click', e => {
+    prevSlide();
+    if(auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+
+//Auto slide
+if(auto) {
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
+
+//Info bar
+burger.addEventListener('click',()=> {
+    slideInfo.classList.toggle('content-active');
+    burger.classList.toggle('toggle');
+});
