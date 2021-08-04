03c0b08f20db154459ce40296777a86086f373ac
Henry Cappel <hencappel1@gmail.com>
2021-08-04 16:35:16 -0500

    adjusting videos for mobile

--- javascript/video-carousel.js
+++ javascript/video-carousel.js
@@ -1,4 +1,18 @@
+var windowWidth = window.matchMedia("(max-width: 800px)")
+
+
 box = document.querySelectorAll('.box');
-box.forEach(popup => popup.addEventListener('click', () => {
-    popup.classList.toggle('active');
-}))
+
+
+function changeVideo(windowWidth) {
+    if (windowWidth.matches) {
+        box[0].classList.add('active');
+    }
+    else {
+        box.forEach(popup => popup.addEventListener('click', () => {
+            popup.classList.toggle('active');
+        }))
+    }
+}
+
+changeVideo(windowWidth);
