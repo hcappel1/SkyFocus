c3ffacdd8aab3b1a402814a5362b8674d84e126f
Henry Cappel <hencappel1@gmail.com>
2021-08-09 11:50:38 -0500

    fixed video container responsive

--- javascript/video-carousel.js
+++ javascript/video-carousel.js
@@ -2,11 +2,27 @@ var windowWidth = window.matchMedia("(max-width: 800px)")
 
 
 box = document.querySelectorAll('.box');
+var i = 0;
 
 
 function changeVideo(windowWidth) {
     if (windowWidth.matches) {
-        box[0].classList.add('active');
+        window.setInterval(function () {
+            if (i == 0) {
+                box[2].classList.remove('active');
+            }
+            else {
+                box[i-1].classList.remove('active');
+            }
+            box[i].classList.add('active');
+            console.log(i);
+            if (i < 2) {
+                i++;
+            }
+            else {
+                i = 0;
+            }
+        }, 5000);
     }
     else {
         box.forEach(popup => popup.addEventListener('click', () => {
